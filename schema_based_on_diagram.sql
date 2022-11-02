39bffc5c03416ec6f760384025137d3fa29a866f
asnganje <abd.nganje@gmail.com>
2022-11-02 13:10:45 +0300

    updated indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -7,7 +7,7 @@ date_of_birth DATE
 CREATE TABLE medical_histories (
 id INT PRIMARY KEY,
 admitted_at TIMESTAMP,
-patient_id INT NULL REFERENCES patients(id) INT,
+medical_histories_patient_id_asc ON medical_histories(patient_id ASC);
 status VARCHAR
 );
 
@@ -16,7 +16,7 @@ id INT PRIMARY KEY,
 total_amount DECIMAL,
 generated_at TIMESTAMP,
 payed_at TIMESTAMP,
-medical_history_id INT NULL REFERENCES medical_histories(id)
+invoices_id ON medical_history(id);
 );
 
 CREATE TABLE invoice_items(
@@ -24,8 +24,8 @@ id INT PRIMARY KEY,
 unit_price DECIMAL,
 quantity INT,
 total_price DECIMAL,
-invoice_id INT NULL REFERENCES invoices(id),
-treatment_id INT NULL REFERENCES treatments(id)
+invoices_items_id ON treatments(id),
+invoices_items_id ON invoices(id)
 );
 
 CREATE TABLE treatments (
@@ -35,16 +35,14 @@ name VARCHAR
 );
 --create JOIN table for many to many relationship
 CREATE TABLE medical_histories_treatments (
-    medical_history_id INT REFERENCES medical_histories(id),
-    treatment_id INT REFERENCES treatments(id)
+    medical_histories_treatments_medical_history_id ON medical_histories_treatments(medical_history_id),
+    medical_histories_treatments_treatment_id ON medical_histories_treatments(treatment_id)
 );
 
 -- Create indexes for improved perfomance
 CREATE INDEX medical_histories_patient_id_asc ON medical_histories(patient_id ASC);
-CREATE INDEX treatments_id ON treatments(id);
-CREATE INDEX invoice_items_id ON invoice_items(id);
-CREATE INDEX invoices_id ON invoices(id);
-CREATE INDEX medical_histories_id ON medical_histories(id);
-CREATE INDEX patients_id ON patients(id);
+CREATE INDEX invoices_id ON medical_history(id);
+CREATE INDEX invoices_items_id ON invoices(id);
+CREATE INDEX invoices_items_id ON treatments(id);
 CREATE INDEX medical_histories_treatments_medical_history_id ON medical_histories_treatments(medical_history_id);
 CREATE INDEX medical_histories_treatments_treatment_id ON medical_histories_treatments(treatment_id);
